156586922c79e0172163308c1a83dc7d39dc7269
supatomic <kngoyal.cs@gmail.com>
2021-06-23 16:06:00 -0400

    deploy and test Color contract

---
+++ test/Color.test.js
@@ -0,0 +1,10 @@
+const Color = artifacts.require('./Color.sol')
+
+require('chai').use(require('chai-as-promised')).should()
+
+contract('Color', (accounts) => {
+    describe('deployment', async () => {
+        it('deploys successfully', async() => {
+        })
+    })
+})
